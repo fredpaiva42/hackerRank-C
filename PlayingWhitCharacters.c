543c1d82d156af8490792a5f9faa04418435e745
fredpaiva42 <fredericogdpaiva@gmail.com>
2023-11-24 12:21:26 -0300

    Playing With Characters Solved!

---
+++ PlayingWhitCharacters.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    char ch, s[100], sen[100];
+    scanf("%c", &ch);
+    scanf(" %s", s);
+    scanf("\n");
+    scanf("%[^\n]%*c", sen);
+
+
+    printf("%c\n", ch);
+    printf("%s\n", s);
+    printf("%s\n", sen);
+
+    return 0;
+}
